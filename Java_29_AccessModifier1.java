a40b6508ab95df042c37ef10d2e43617e622971a
Sandesh Khatiwada <Khatiwadasandesh501@gmail.com>
2025-08-07 21:38:18 +0545

    Java 29

---
+++ Java_29_AccessModifier1.java
@@ -0,0 +1,8 @@
+
+public class Java_29_AccessModifier1 {
+    public int a = 5;
+    protected int b = 10;
+    int c = 15;
+    // private int d = 20;--> it can only be accessed with a publuc function
+
+}
